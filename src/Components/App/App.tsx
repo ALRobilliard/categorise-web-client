6411321aac90cc44e0b1e7d352c6d595431ac107
Andrew Robilliard <robilliardnz@gmail.com>
2020-08-23 19:36:48 +1200

    Configure base page

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from '../../logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import RouterView from '../RouterView/RouterView';
 import './App.css';
+import SignIn from '../SignIn/SignIn';
+import SideNavigation from '../SideNavigation/SideNavigation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IProps {
+  // No props as this is the top-level component.
+}
+
+interface IState {
+  user?: UserDto;
+}
+
+class App extends Component<IProps, IState> {
+  state: IState;
+
+  constructor(props: IProps) {
+    super(props);
+    this.state = {};
+  }
+
+  refreshUser = () => {
+    const user: UserDto = {
+      id: sessionStorage.getItem('userId') as string,
+      firstName: sessionStorage.getItem('userFirstName') as string,
+      lastName: sessionStorage.getItem('userLastName') as string,
+      email: sessionStorage.getItem('userEmail') as string,
+      token: sessionStorage.getItem('userToken') as string,
+    };
+
+    if (user.token == null) {
+      this.setState({ user: undefined });
+      return false;
+    }
+
+    this.setState({ user });
+    return true;
+  };
+
+  clearUser = () => {
+    sessionStorage.removeItem('userId');
+    sessionStorage.removeItem('userFirstName');
+    sessionStorage.removeItem('userLastName');
+    sessionStorage.removeItem('userEmail');
+    sessionStorage.removeItem('userToken');
+
+    this.refreshUser();
+  };
+
+  componentWillMount() {
+    this.refreshUser();
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Router>
+          <div className='sidebar'>
+            <SideNavigation user={this.state.user} clearUser={this.clearUser} />
+          </div>
+          <div className='main'>
+            <RouterView user={this.state.user} refreshUser={this.refreshUser} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
